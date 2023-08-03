167dbb5807c6271b69433e88dc5685ad97ad20fc
PAKISTANI HACKER GROUP <126904105+pakistanihackergroup@users.noreply.github.com>
2023-08-03 23:14:27 +0300

    Update errors.go

--- web/errors.go
+++ web/errors.go
@@ -1,1 +1,11 @@
+package web
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pakistanihackergroup/phone-number-information/v2/web/errors"
+)
+
+func handleError(c *gin.Context, e *errors.Error) {
+	c.JSON(e.Status(), JSONResponse{Success: false, Error: e.String()})
+	c.Abort()
+}
